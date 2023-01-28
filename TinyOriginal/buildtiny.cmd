62faf4e6d5e4f6773664b2473a6360c51e78b6f8
Charles Stevens <chazste@yahoo.com>
2023-01-28 12:23:54 -0600

    Modify batch file to also link using ms link.
    Modify batch file to assemble and link using both mslink
    and crinkler (optinally) and document some issues with
    linking.
    
    Signed-off-by: Charles Stevens <chazste@yahoo.com>

--- TinyOriginal/buildtiny.cmd
+++ TinyOriginal/buildtiny.cmd
@@ -1,4 +1,25 @@
-ml /c /coff /IC:\masm32\include .\Tiny.asm 
-crinkler.exe /ENTRY:MainEntry /SUBSYSTEM:WINDOWS /TINYHEADER /NOINITIALIZERS /UNSAFEIMPORT /ORDERTRIES:2000 /TINYIMPORT /LIBPATH:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.20348.0\um\x86" kernel32.lib user32.lib gdi32.lib Tiny.obj /OUT:tiny.exe
+@setlocal
 
+@rem Set LIBDIR to the directory containing the import libs for 
+@rem kernel32 gdi32 and user32. You must use the SDK import libraries 
+@rem for crinkler to work. If you use the masm32 libs the link
+@rem version will be 32 bytes smaller.
 
+@set LIBDIR="C:\Program Files (x86)\Windows Kits\10\\lib\10.0.22000.0\um\x86"
+@rem @set LIBDIR="C:\masm32\lib"
+
+@if not exist %LIBDIR%\user32.lib (
+	@echo Set SDK LIB correctly
+	exit /b 1
+)
+
+@rem Assemble
+ml /c /coff /IC:\masm32\include Tiny.asm 
+
+@rem Link with the Microsoft linker
+link /LIBPATH:%LIBDIR% /NODEFAULTLIB:LIBCMT.lib /ENTRY:MainEntry /DEBUG:NONE /subsystem:windows /map /merge:.rdata=.text /merge:.data=.text /align:16 gdi32.lib user32.lib kernel32.lib tiny.obj
+
+@rem Link using crinkler if the 'c' option passed on command line
+if xc == x%1 (
+	..\crinkler\crinkler.exe /LIBPATH:%LIBDIR% /ENTRY:MainEntry /SUBSYSTEM:WINDOWS /TINYHEADER /NOINITIALIZERS /UNSAFEIMPORT /ORDERTRIES:2000 /TINYIMPORT tiny.obj kernel32.lib user32.lib gdi32.lib /OUT:tiny-cr.exe
+)
